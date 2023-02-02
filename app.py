94882b75ef400150840e3435a4043047b3f7baaf
kyohei seino <seino0702@gmail.com>
2023-02-02 11:00:18 +0900

    環境構築

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from sqlalchemy.orm import declarative_base, relationship
 
 app = Flask(__name__)
 #dbのURLを設定
-db_uri = os.environ.get('DATABASE_URL') or "postgresql://postgres:tanukitanu99@localhost/work_app"
+db_uri = os.environ.get('DATABASE_URL') or "postgresql://postgres:postgres@localhost/work_app"
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri 
 db = SQLAlchemy(app) 
 
